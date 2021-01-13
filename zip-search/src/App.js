adb134283c15055df65bcf433d91d25e4812606f
richardljohn <richardljohn@gmail.com>
2021-01-13 14:25:30 -0500

    CodeSandBox.io Link

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return(
     <FetchZipsResult/>
   );
+  //Link to codesandbox.io: https://codesandbox.io/s/proud-snowflake-d57kg?file=/zip-search/src/components/fetchZips.js
 }
 
 export default App;
